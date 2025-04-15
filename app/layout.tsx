f1b31d8e8b5fee3bf44a49636ad593dcad7fc1d0
mohammedyasar001 <yasar2052002@gmail.com>
2025-04-15 12:54:51 +0530

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/next';
+
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -22,6 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
